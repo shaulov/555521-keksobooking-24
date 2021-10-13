fd350efa0637669906c8aa64033f7bdc4c6e4a7c
shaulov <ivan.shaulov@mail.ru>
2021-10-13 16:25:26 +0300

    added arrays of data and functions for create array of similar ads object

--- js/main.js
+++ js/main.js
@@ -1,19 +1,136 @@
-function getRandomPositiveFloat(firstNumber, secondNumber, decimalPlaces = 1) {
-  const lower = Math.min(Math.abs(firstNumber), Math.abs(secondNumber));
-  const upper = Math.max(Math.abs(firstNumber), Math.abs(secondNumber));
+import {randomPositiveFloat} from './utils/get-random-positive-float';
+import {randomPositiveInteger} from './utils/get-random-positive-integer';
 
-  const result = Math.random() * (upper - lower) + lower;
+'use strict';
 
-  return result.toFixed(decimalPlaces);
+const AUTHOR_AVATARS = [
+  'img/avatars/user01.png',
+  'img/avatars/user02.png',
+  'img/avatars/user03.png',
+  'img/avatars/user04.png',
+  'img/avatars/user05.png',
+  'img/avatars/user06.png',
+  'img/avatars/user07.png',
+  'img/avatars/user08.png',
+  'img/avatars/user09.png',
+  'img/avatars/user10.png',
+];
+
+const OFFER_TITLES = [
+  'Двухэтажный коттедж',
+  'Трехкомнатные апартаменты',
+  'Светлая двухкомнатная квартира',
+  'Пляжный домик',
+  'Просторная комната',
+  'Двухкомнатная квартира',
+  'Светлая однушка',
+  'Уютная студия',
+  'Однушка с утепленной лоджией',
+  'Роскошные апартаменты',
+];
+
+const OFFER_TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel',
+];
+
+const OFFER_CHECKTIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const OFFER_FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const OFFER_PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const OFFER_DESCRIPTIONS = [
+  'Просторное светлое жилье сбольшим количеством спальных мест.',
+  'Отличные апартаменты с видом на закат.',
+  'Уютная квартира с джакузи.',
+  'Однушка с утепленной лоджией в развитом районе.',
+  'Пляжный домик с очень красивым видом.',
+  'Отличные апартаменты в развитом районе.',
+  'Просторное светлое жилье с джакузи.',
+  'Трехкомнатные апартаменты в центре города.',
+  'Двухэтажный коттедж в тихой лесистой местности',
+  'Отличные апартаменты с очень красивым видом.',
+];
+
+const SIMILAR_AD_COUNT = 10;
+const MIN_LOCATION_COORDINATE = 35.65000;
+const MAX_LOCATION_COORDINATE = 35.70000;
+const DECIMAL_POINTS_COUNT = 5;
+const MIN_PRICE = 1000;
+const MAX_PRICE = 10000;
+const MIN_ROOMS_COUNT = 1;
+const MAX_ROOMS_COUNT = 5;
+const MIN_GUESTS_COUNT = 1;
+const MAX_GUESTS_COUNT = 7;
+
+const getAuthorAvatar = (number) => {
+  return AUTHOR_AVATARS[number];
+};
+
+const getRandomArrayElement = (array) => {
+  return array[randomPositiveInteger(0, array.length - 1)];
+};
+
+const getOfferFeatures = () => {
+  let features = OFFER_FEATURES.slice();
+  for (let i = 0; i <randomPositiveInteger(0, OFFER_FEATURES.length - 1); i++) {
+    features.splice(randomPositiveInteger(0, OFFER_FEATURES.length - 1), 1);
+  }
+  return features;
+};
+
+const getOfferPhotos = () => {
+  return Array.from({length: Math.floor(Math.random() * 10)}, () => OFFER_PHOTOS[randomPositiveInteger(0, OFFER_PHOTOS.length - 1)]);
 }
-function getRandomPositiveInteger(firstNumber, secondNumber) {
-  const lower = Math.min(Math.abs(firstNumber), Math.abs(secondNumber));
-  const upper = Math.max(Math.abs(firstNumber), Math.abs(secondNumber));
 
-  const result = Math.random() * (upper - lower) + lower;
+const createSimilarAdNearby = (numberOfAd) => {
 
-  return result;
+  return {
+    author: {
+      avatar: getAuthorAvatar(numberOfAd),
+    },
+    offer: {
+      title: getRandomArrayElement(OFFER_TITLES),
+      address: '',
+      price: randomPositiveInteger(MIN_PRICE, MAX_PRICE),
+      type: getRandomArrayElement(OFFER_TYPES),
+      rooms: randomPositiveInteger(MIN_ROOMS_COUNT, MAX_ROOMS_COUNT),
+      guests: randomPositiveInteger(MIN_GUESTS_COUNT, MAX_GUESTS_COUNT),
+      checkin: getRandomArrayElement(OFFER_CHECKTIMES),
+      checkout: getRandomArrayElement(OFFER_CHECKTIMES),
+      features: getOfferFeatures(),
+      description: getRandomArrayElement(OFFER_DESCRIPTIONS),
+      photos: getOfferPhotos(),
+    },
+    location: {
+      lat: randomPositiveFloat(MIN_LOCATION_COORDINATE, MAX_LOCATION_COORDINATE, DECIMAL_POINTS_COUNT),
+      lng: randomPositiveFloat(MIN_LOCATION_COORDINATE, MAX_LOCATION_COORDINATE, DECIMAL_POINTS_COUNT),
+    },
+  }
 }
 
-getRandomPositiveFloat();
-getRandomPositiveInteger();
+const SIMILAR_AD = [];
+for (let currentObject = 0; currentObject < SIMILAR_AD_COUNT; currentObject++) {
+  SIMILAR_AD.push(createSimilarAdNearby(currentObject));
+  SIMILAR_AD[currentObject]['offer'].address = `${SIMILAR_AD[currentObject]['location'].lat}, ${SIMILAR_AD[currentObject]['location'].lng}`;
+}
+console.log(SIMILAR_AD);
