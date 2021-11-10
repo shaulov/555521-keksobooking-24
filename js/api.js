3fdba2039bee056cae3a5b9bae284f682287a1af
shaulov <ivan.shaulov@mail.ru>
2021-11-10 18:26:24 +0300

    created module for work with api

---
+++ js/api.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const getData = (onSuccess, onError) => {
+  fetch('https://24.javascript.pages.academy/keksobooking/data')
+    .then((response) => {
+      if(response.ok) {
+        return response.json()
+      }
+
+      throw new Error(`${response.status} - ${response.statusText}`);
+    })
+    .then((data) => {
+      onSuccess(data);
+    })
+    .catch((err) => {
+      onError(err);
+    });
+};
+
+const sendData = (onSuccess, onError, body) => {
+  fetch(
+    'https://24.javascript.pages.academy/keksobooking',
+    {
+      method: 'POST',
+      body,
+    }
+  )
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        onError();
+      }
+    })
+    .catch(() => {
+      onError();
+    });
+}
+
+export {getData, sendData};
