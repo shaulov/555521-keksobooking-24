295b17ea3b282cb2f721bf9c2e048aaa37836837
shaulov <ivan.shaulov@mail.ru>
2021-11-10 18:22:43 +0300

    added code for init project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 import './popup.js';
-import './form-validate.js';
-import './map.js';
+import {setFormSubmit, setResetForm} from './form-validate.js';
+import {createSimilarAdsOnMap} from './map.js';
+import {getData} from './api.js';
+import {showAlert} from './util.js'
+import {showSuccessMessage, showErrorMessage} from './modal.js'
+'use strict';
+
+const SIMILAR_AD_COUNT = 10;
+
+getData(
+  (ads) => {
+    createSimilarAdsOnMap(ads.slice(0, SIMILAR_AD_COUNT));
+  },
+  () => showAlert('Произошла ошибка при загрузке похожих объявлений. Попробуйте обновить страницу позже')
+);
+
+setFormSubmit(showSuccessMessage, showErrorMessage);
+setResetForm();
